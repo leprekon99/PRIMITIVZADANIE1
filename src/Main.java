1cbbd6e2d9b36a424972eb8b89cc20e074f99592
neto test <babenko.eliz@yandex.ru>
2023-06-22 12:53:35 +0300

    PRIMITIVZADANIE1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountTicket = 13676;
+        int amountOneBonus = 20;
+
+        System.out.println("Количество Ваших бонусов составляет:");
+        System.out.println((amountTicket / amountOneBonus) + " бонусов");
+
+    }
+}
